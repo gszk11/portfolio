f800600571fe8469dfad4a47be7c6f1bd8f385bc
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-14 08:05:14 +0000

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  attachment :profile_image
+  
+  has_many :movies, dependent: :destroy
+  has_many :movie_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+end
