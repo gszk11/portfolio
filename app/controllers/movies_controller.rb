9995bfe10d893c26d1a63416d8edecd3bc8c585c
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-16 08:34:45 +0000

    add movie-new page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,14 @@ class MoviesController < ApplicationController
   end
 
   def new
+    @movie = Movie.new
   end
 
   def edit
+  end
+  
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :body, :genre)
   end
 end
