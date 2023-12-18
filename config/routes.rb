b6467df3fa2048673d3cba4dddcbb19f06bd2e3f
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-18 06:56:41 +0000

    add movie-index-search method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
+  get 'movie/search', to: 'movies#search'
   resources :users, only: [:index, :show, :edit, :update]
   resources :movies, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
