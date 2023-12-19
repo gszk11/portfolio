06103e686a47e21292aa328106ff9e1974bf48b2
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-19 06:03:47 +0000

    add moviecomment create and destroy method

--- app/controllers/movie_comments_controller.rb
+++ app/controllers/movie_comments_controller.rb
@@ -1,8 +1,20 @@
 class MovieCommentsController < ApplicationController
   
   def create
+    movie = Movie.find(params[:movie_id])
+    comment = MovieComment.new(movie_params)
+    comment.user_id = current_user.id
+    comment.save
+    redirect_to movie_path(movie.id)
   end
   
   def destroy
+    MovieComment.find_by(id: params[:id]).destroy
+    redirect_to movie_path(params[:movie_id])
+  end
+  
+  private
+  def movie_comment_params
+    params.require(:movie_comment).permit(:comment)
   end
 end
