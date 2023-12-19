2ebc92c825a155ac78f19bff779849ab990e45ba
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-19 06:01:25 +0000

    add favorite create and destroy method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,16 @@
 class FavoritesController < ApplicationController
   
   def create
+    movie = Movie.find(params[:movie_id])
+    favorite = current_user.favorites.new(movie_id: movie.id)
+    favorite.save
+    redirect_to movie_path(movie.id)
   end
 
   def destroy
+    movie = Movie.find(params[:movie_id])
+    favorite = current_user.favorites.find_by(movie_id: movie.id)
+    favorite.destroy
+    redirect_to movie_path(movie.id)
   end
 end
