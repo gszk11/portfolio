58578b0a39e5aa450d7ae25cfa0245ec6b4c886e
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-19 06:01:25 +0000

    add favorite create and destroy method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :user
   has_many :movie_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
-  validates :title, presence: true
-  validates :body, presence: true, length: { maximum:200 }
-  validates :genre, presence: true
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+  
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum:200 }
+  validates :genre, presence: true
 end
