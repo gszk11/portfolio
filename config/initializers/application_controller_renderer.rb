9cfaa708ea17788ba098d20cc226edcc3ab3c6aa
gszk11 <147461379+gszk11@users.noreply.github.com>
2023-12-22 07:12:57 +0000

    add refile secretkey

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c083e6b19d059529e4c9b1c44e91bd04bac65dd81e98b04f8bad39f5e8269a1c76ad478760452f62d6d9f3322a6d5a91d1b885863e31f4d49b06303a1a2d89c6'
